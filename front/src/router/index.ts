5b88c840e67db36f65ddd7790127c7fa36e9c0b7
Tomonori Yamada <kuronekodai15@gmail.com>
2021-05-29 18:40:54 +0900

    minimum_implementation

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import codeCheck from "@/views/codeCheck/index.vue"
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes: Array<RouteConfig> = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/codeCheck",
+    name: "codeCheck",
+    component: codeCheck,
   },
   {
     path: "/about",

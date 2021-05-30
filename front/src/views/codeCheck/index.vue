83ee4fccb1a236415d785009e185ba573a46826c
Tomonori Yamada <kuronekodai15@gmail.com>
2021-05-30 14:55:57 +0900

    add box

--- front/src/views/codeCheck/index.vue
+++ front/src/views/codeCheck/index.vue
@@ -1,10 +1,10 @@
 <template>
-
-
 <div>
   <textarea class="textarea margin-textarea" rows="10" v-model="code" placeholder="質問したいプログラムを入力してください"></textarea>
-  <div class="code-layout" v-for="codeElement in codeArray">
-    <comment :codeElement="codeElement"></comment>
+  <div class="box" v-if="codeArray.length > 0">
+    <div class="code-layout" v-for="codeElement in codeArray">
+      <comment :codeElement="codeElement"></comment>
+    </div>
   </div>
 </div>
 </template>
@@ -17,7 +17,7 @@
   white-space: pre-wrap; 
 }
 .margin-textarea{
-  margin: 2em
+  margin: 2em 0em
 }
 </style>
 
